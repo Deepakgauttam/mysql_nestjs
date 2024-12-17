3efa7b6efe1482a1a01d566fbbb32a1a030fd847
Deepakgauttam <105534881+Deepakgauttam@users.noreply.github.com>
2024-12-17 23:20:05 +0530

    Console the body

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{ logger: ['log', 'error', 'warn', 'debug', 'verbose'] });
   // Enable global validation
   app.useGlobalPipes(new ValidationPipe());
 
